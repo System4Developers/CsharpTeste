31e4afa12835cfee362d1a62faeb5c6a09a8df16
njunior2593 <neelson_jr@live.com>
2019-06-06 22:39:38 -0300

    final

--- Aplicacao/s4d_biomedicina/s4d_biomedicina/Apresentacao/frmPacientesExames.Designer.cs
+++ Aplicacao/s4d_biomedicina/s4d_biomedicina/Apresentacao/frmPacientesExames.Designer.cs
@@ -28,42 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnEditar = new System.Windows.Forms.Button();
             this.dgvPacientesExames = new System.Windows.Forms.DataGridView();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.lblGeral = new MaterialSkin.Controls.MaterialLabel();
+            this.btnEditar = new MaterialSkin.Controls.MaterialRaisedButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPacientesExames)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.Location = new System.Drawing.Point(937, 328);
-            this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(136, 28);
-            this.btnEditar.TabIndex = 17;
-            this.btnEditar.Text = "Editar Exames";
-            this.btnEditar.UseVisualStyleBackColor = true;
-            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // dgvPacientesExames
             // 
             this.dgvPacientesExames.AllowUserToAddRows = false;
             this.dgvPacientesExames.AllowUserToDeleteRows = false;
             this.dgvPacientesExames.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPacientesExames.Location = new System.Drawing.Point(25, 21);
+            this.dgvPacientesExames.Location = new System.Drawing.Point(25, 86);
             this.dgvPacientesExames.Margin = new System.Windows.Forms.Padding(2);
             this.dgvPacientesExames.Name = "dgvPacientesExames";
             this.dgvPacientesExames.ReadOnly = true;
             this.dgvPacientesExames.RowTemplate.Height = 24;
-            this.dgvPacientesExames.Size = new System.Drawing.Size(1048, 291);
+            this.dgvPacientesExames.Size = new System.Drawing.Size(1057, 327);
             this.dgvPacientesExames.TabIndex = 16;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(32, 67);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(1050, 2);
+            this.materialDivider1.TabIndex = 56;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // lblGeral
+            // 
+            this.lblGeral.AutoSize = true;
+            this.lblGeral.Depth = 0;
+            this.lblGeral.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblGeral.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblGeral.Location = new System.Drawing.Point(40, 33);
+            this.lblGeral.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblGeral.Name = "lblGeral";
+            this.lblGeral.Size = new System.Drawing.Size(68, 19);
+            this.lblGeral.TabIndex = 55;
+            this.lblGeral.Text = "EXAMES";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditar.Depth = 0;
+            this.btnEditar.Location = new System.Drawing.Point(891, 427);
+            this.btnEditar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Primary = true;
+            this.btnEditar.Size = new System.Drawing.Size(170, 29);
+            this.btnEditar.TabIndex = 57;
+            this.btnEditar.Text = "EDITAR EXAMES";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // frmPacientesExames
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.ClientSize = new System.Drawing.Size(1112, 388);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1112, 468);
             this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.lblGeral);
             this.Controls.Add(this.dgvPacientesExames);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPacientesExames";
@@ -72,12 +104,14 @@
             this.Load += new System.EventHandler(this.frmPacientesExames_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPacientesExames)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.DataGridView dgvPacientesExames;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialLabel lblGeral;
+        private MaterialSkin.Controls.MaterialRaisedButton btnEditar;
     }
 }
